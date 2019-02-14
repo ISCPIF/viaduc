d9755a74fecab1160d52065d32d09933be92e8cc
Romain Reuillon <romain.reuillon@iscpif.fr>
2019-02-14 15:55:24 +0100

    Update scala and sbt

--- client/src/main/scala/fr/iscpif/client/ACT_ODE.java
+++ client/src/main/scala/fr/iscpif/client/ACT_ODE.java
@@ -1,23 +1,23 @@
-package fr.iscpif.client;
-import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
-
-public  class ACT_ODE implements FirstOrderDifferentialEquations {
-
-    private double[] c;
-    private double omega;
-
-    public void CircleODE(double[] c, double omega) {
-        this.c     = c;
-        this.omega = omega;
-    }
-
-    public int getDimension() {
-        return 2;
-    }
-
-    public void computeDerivatives(double t, double[] y, double[] yDot) {
-        yDot[0] = omega * (c[1] - y[1]);
-        yDot[1] = omega * (y[0] - c[0]);
-    }
-
-}
+//package fr.iscpif.client;
+//import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
+//
+//public  class ACT_ODE implements FirstOrderDifferentialEquations {
+//
+//    private double[] c;
+//    private double omega;
+//
+//    public void CircleODE(double[] c, double omega) {
+//        this.c     = c;
+//        this.omega = omega;
+//    }
+//
+//    public int getDimension() {
+//        return 2;
+//    }
+//
+//    public void computeDerivatives(double t, double[] y, double[] yDot) {
+//        yDot[0] = omega * (c[1] - y[1]);
+//        yDot[1] = omega * (y[0] - c[0]);
+//    }
+//
+//}
--- project/build.sbt
+++ project/build.sbt
@@ -2,6 +2,6 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 addSbtPlugin("fr.iscpif" % "scalajs-execnpm" % "0.4")
 
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.22")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.26")
 
 addSbtPlugin("org.scalatra.sbt" % "sbt-scalatra" % "1.0.1")
